50fd2b7f055edafb0219916f1e77f42e086302f9
Ramiro Gustavo Lopez <ramlopez@everis.com>
2020-08-18 20:57:52 -0300

    Added frontend app as build folder

---
+++ build/precache-manifest.5873c6bcc6f7e931ffd76dd4043b3f60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9babce10564b04dbe91d069ec3013000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d3be29c530045eebd4",
+    "url": "/static/js/main.5395964e.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
